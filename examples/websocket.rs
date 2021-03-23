3bf24f7bb7b9872cf2188cb491688d2db98ea824
Noah <noah@coronasoftware.net>
2021-03-22 17:55:44 -0700

    Cleanup websocket example

--- examples/websocket.rs
+++ examples/websocket.rs
@@ -18,24 +18,21 @@ impl Actor for Server {
         Self
     }
 
-    fn handle(&mut self, stream: Self::Input, link: &Link<Self>) {
-        let mut ws = server::accept(stream).expect("Error creating WS");
-        link.respond(()).ok();
+    fn handle(&mut self, stream: Self::Input, _link: &Link<Self>) {
+        let mut ws = server::accept(stream).unwrap();
         loop {
             let msg = ws.read_message();
             match msg {
                 Ok(tungstenite::Message::Binary(msg)) => {
-                    let msg =
-                        bincode::deserialize::<Message>(&msg).expect("Error deserializing message");
+                    let msg = bincode::deserialize::<Message>(&msg).unwrap();
 
                     let response = match msg {
                         Message::Ping => Message::Pong,
                         Message::Pong => Message::Ping,
                     };
 
-                    let buf = bincode::serialize(&response).expect("Error serializing response");
-                    ws.write_message(buf.into())
-                        .expect("Error sending response");
+                    let buf = bincode::serialize(&response).unwrap();
+                    ws.write_message(buf.into()).unwrap();
                 }
                 Err(_) => break,
                 _ => {}
@@ -44,27 +41,23 @@ impl Actor for Server {
     }
 }
 
-struct Listener {
-    listener: TcpListener,
-}
-
+struct Listener;
 impl Actor for Listener {
-    type Input = ();
+    type Input = String;
     type Output = ();
 
     fn create() -> Self {
-        Self {
-            listener: TcpListener::bind("127.0.0.1:6000").expect("Could not bind"),
-        }
+        Self
     }
 
-    fn handle(&mut self, _msg: Self::Input, link: &Link<Self>) {
+    fn handle(&mut self, addr: Self::Input, link: &Link<Self>) {
+        let listener = TcpListener::bind(&addr).expect("Failed to bind");
+        // Notify we're ready to accept connections.
         link.respond(()).unwrap();
         loop {
-            if let Ok(stream) = self.listener.accept() {
-                wactor::spawn::<Server>()
-                    .get(stream)
-                    .expect("Error spawning node")
+            if let Ok(stream) = listener.accept() {
+                // Spawn a server for this connection
+                wactor::spawn::<Server>().send(stream).unwrap()
             }
         }
     }
@@ -74,24 +67,25 @@ fn main() {
     // Start listening
     wactor::spawn::<Listener>()
         // Wait until its ready
-        .get(())
-        .expect("Listener failed to start");
+        .get("127.0.0.1:6000".to_string())
+        .unwrap();
 
     // Connect to our server.
     let client = TcpStream::connect("127.0.0.1:6000").unwrap();
     let (mut socket, _response) =
-        tungstenite::client("ws://127.0.0.1:6000", client).expect("Can't connect");
+        tungstenite::client("ws://127.0.0.1:6000", client).expect("Failed to connect");
 
     // Let the ping pong begin!
     let buf = bincode::serialize(&Message::Ping).unwrap();
     socket.write_message(buf.into()).unwrap();
     loop {
-        let msg = socket.read_message().expect("Error reading message");
+        let msg = socket.read_message().unwrap();
         if msg.is_binary() {
             let answer = bincode::deserialize::<Message>(&msg.into_data()).unwrap();
+
             println!("{:?}", answer);
 
-            let buf = bincode::serialize(&answer).expect("Error serializing response");
+            let buf = bincode::serialize(&answer).unwrap();
             socket.write_message(buf.into()).unwrap();
         }
     }
